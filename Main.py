258321e74ffef33f7eac6c4d0e619e899588feef
cwj980119 <cwj980119@g.hongik.ac.kr>
2022-06-16 00:00:38 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ from PyQt5 import uic,QtWidgets, QtGui
 from keras.models import load_model
 import pymysql
 
-
 def connectDB():
     host="database-1.cb5pctivsgrb.us-east-1.rds.amazonaws.com"
     username="root"
@@ -26,6 +25,8 @@ class Main(QWidget):
         self.ui = uic.loadUi("./UI/main.ui")
         self.ui.show()
         self.ui.btn_login.clicked.connect(self.toLogin)
+        self.ui.btn_register.clicked.connect(self.a)
+
 
     def toLogin(self):
         self.ui.hide()
@@ -33,6 +34,8 @@ class Main(QWidget):
 
     def toMain(self):
         self.ui.show()
+    def a(self):
+        self.b = Check()
 
 class Login(QWidget):
     def __init__(self, main):
@@ -89,23 +92,33 @@ class Login(QWidget):
                     count+=1
                     user_list[np.argmax(a)] += 1
                     #print(np.argmax(a),"th user")
-                    if count>20:
-                        print(user_list)
-                        a = user_list
-                        a.sort(reverse=True)
-                        self.close_cam()
-                        print(a[0])
-                        print(user_list)
-                        print(user_list.index(a[0]))
-                    #print(a)
-                    
+                    # if count>20:
+                    #     print(user_list)
+                    #     a = user_list.copy()
+                    #     a.sort(reverse=True)
+                    #     #self.check = Check()
+                    #     self.close_cam()
+                    #     print(a[0])
+                    #     print(user_list)
+                    #     print(user_list.index(a[0]))
+                    # #print(a)
 
+            print("done")
             cvt_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             h, w, c = cvt_frame.shape
             qImg = QtGui.QImage(cvt_frame.data, w, h, w * c, QtGui.QImage.Format_RGB888)
             pixmap = QtGui.QPixmap.fromImage(qImg)
             self.ui.cam.resize(round(width), round(height))
             self.ui.cam.setPixmap(pixmap)
+            if count > 20:
+                print(user_list)
+                a = user_list.copy()
+                a.sort(reverse=True)
+                print(a[0])
+                print(user_list)
+                print(user_list.index(a[0]))
+                self.close()
+        self.success_login()
 
     def start_cam(self):
         print("start")
@@ -118,8 +131,23 @@ class Login(QWidget):
 
     def success_login(self):
         self.close_cam()
- 
-     
+        th = threading.Thread(target=self.start_check)
+        th.start()
+
+    def start_check(self):
+        self.check =Check()
+
+class Check(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.ui = uic.loadUi("./UI/check.ui")
+        self.ui.show()
+        self.ui.btn_yes.clicked.connect(self.answer_yes)
+        print("check init")
+
+    def answer_yes(self):
+        print("yes")
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
